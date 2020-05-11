5047ac732b47a507dbc2a498321cd70014cefcb0
Slava <slavken@outlook.com>
2020-05-12 00:14:36 +0300

    Update scripts.blade.php

--- resources/views/includes/scripts.blade.php
+++ resources/views/includes/scripts.blade.php
@@ -1,4 +1,4 @@
-<script src="js/app.js"></script>
+<script src="{{ asset('js/app.js') }}"></script>
 <script src="https://cdn.ckeditor.com/ckeditor5/16.0.0/classic/ckeditor.js"></script>
 <script>
     var editor = document.querySelector('#editor');
